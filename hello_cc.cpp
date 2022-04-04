f0ecde50ea0717738aec455785bd30eac3bde947
xiaolikai <xiaolikai96@163.com>
2022-04-04 21:38:13 +0800

    add cpp file

---
+++ hello_cc.cpp
@@ -0,0 +1,16 @@
+/*
+编译过程
+
+1   g++ -o hello_cc hello_cc.cpp
+    生成可执行文件  hello_cc
+2   ./hello_cc
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "hello, cpp" << endl;
+    return 0;
+}
